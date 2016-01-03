3591dca71a12425b065158a2766ef295e38d04af
Tre Murillo <treelzebub@gmail.com>
2016-01-02 18:29:02 -0800

    rx login activity

--- app/src/main/java/net/treelzebub/zinepress/ui/activity/LoginActivity.kt
+++ app/src/main/java/net/treelzebub/zinepress/ui/activity/LoginActivity.kt
@@ -1,11 +1,17 @@
 package net.treelzebub.zinepress.ui.activity
 
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import butterknife.bindView
-import net.treelzebub.zinepress.Constants
 import net.treelzebub.zinepress.R
+import net.treelzebub.zinepress.auth.PocketTokenManager
+import net.treelzebub.zinepress.util.ToastUtils
+import rx.android.lifecycle.LifecycleObservable
+import rx.android.schedulers.AndroidSchedulers
 
 /**
  * Created by Tre Murillo on 1/2/16
@@ -18,17 +24,40 @@ class LoginActivity : BaseRxActivity() {
         setContentView(R.layout.activity_login)
         val wvSettings = webView.settings
         wvSettings.builtInZoomControls = true
-        wvSettings.javaScriptEnabled = true
-        webView.setWebViewClient(RequestTokenCallback())
+        wvSettings.javaScriptEnabled   = true
+        authorize()
     }
 
-    private inner class RequestTokenCallback : WebViewClient() {
-        override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            if (!url.isNullOrBlank() && url.startsWith(Constants.REDIRECT_URI)) {
-//                AuthUtils.requestAccessToken(this@LoginActivity, Uri.parse(url))
-                return true
+    private fun authorize() {
+        val manager = PocketTokenManager.from(this)
+        val grant   = manager.newAuthCodeGrant()
+
+        // Listen for the auth url to load
+        LifecycleObservable.bindActivityLifecycle(lifecycle(), grant.authorizationUri())
+                .map { it.toString() }
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe { webView.loadUrl(it) }
+
+        webView.setWebViewClient(object : WebViewClient() {
+            override fun onPageStarted(view: WebView, url: String, favicon: Bitmap?) {
+                grant.onUriLoadedCommand.onNext(Uri.parse(url))
+                // Hide redirect page from user
+                if (url.startsWith(grant.redirectUri)) {
+                    webView.visibility = View.GONE
+                }
             }
-            return false
-        }
+        })
+
+        // Begin auth and listen for success
+        LifecycleObservable.bindActivityLifecycle(lifecycle(), manager.grantNewAccessToken(grant))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    if (it != null) {
+                        setResult(RESULT_OK)
+                        finish()
+                    } else {
+                        ToastUtils.show(this, "Error.") //TODO
+                    }
+                }
     }
 }
--- app/src/main/java/net/treelzebub/zinepress/util/AuthUtils.kt
+++
@@ -1,8 +0,0 @@
-package net.treelzebub.zinepress.util
-
-/**
- * Created by Tre Murillo on 1/2/16
- */
-object AuthUtils {
-
-}
