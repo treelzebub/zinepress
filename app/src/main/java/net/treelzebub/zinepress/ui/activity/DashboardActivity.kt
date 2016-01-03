fbb933ce5d8b1a26e6747b5109d2dea96cb62912
Tre Murillo <treelzebub@gmail.com>
2016-01-02 22:47:27 -0800

    dashboard basics

--- app/src/main/java/net/treelzebub/zinepress/ui/activity/DashboardActivity.kt
+++ app/src/main/java/net/treelzebub/zinepress/ui/activity/DashboardActivity.kt
@@ -1,8 +1,68 @@
 package net.treelzebub.zinepress.ui.activity
 
+import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import net.treelzebub.zinepress.R
+
+import kotlinx.android.synthetic.main.activity_dashboard.nav_view as navView
+import kotlinx.android.synthetic.main.activity_dashboard.drawer_layout as drawer
+import kotlinx.android.synthetic.main.app_bar_dashboard.*
+
 /**
  * Created by Tre Murillo on 1/2/16
  */
-class DashboardActivity : BaseRxActivity() {
+class DashboardActivity : BaseRxActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener {
+            Snackbar.make(it, "Do some shit", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+        }
+        val toggle = ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.setDrawerListener(toggle)
+        toggle.syncState()
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.dashboard, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else                 -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_camera     -> {}
+            R.id.nav_gallery    -> {}
+            R.id.nav_slideshow  -> {}
+            R.id.nav_manage     -> {}
+            R.id.nav_share      -> {}
+            R.id.nav_send       -> {}
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
