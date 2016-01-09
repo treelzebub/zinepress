ce323161f27e0caf52bb7dadf2c02e5e232a85a9
Tre Murillo <treelzebub@gmail.com>
2016-01-08 20:24:27 -0800

    content provider

--- app/src/main/java/net/treelzebub/zinepress/zine/ZineArticle.kt
+++ app/src/main/java/net/treelzebub/zinepress/zine/ZineArticle.kt
@@ -1,8 +1,10 @@
 package net.treelzebub.zinepress.zine
 
+import java.io.Serializable
+
 /**
  * Created by Tre Murillo on 1/3/16
  */
 class ZineArticle(val url: String,
               val title: String,
-              val rawHtml: String)
+              val rawHtml: String) : Serializable
