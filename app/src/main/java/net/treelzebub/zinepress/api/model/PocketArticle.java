83e305b3689f148f3cf3d70764e45c2f201a465a
Tre Murillo <treelzebub@gmail.com>
2016-02-20 10:09:39 -0800

    fix PocketResponse schema

--- app/src/main/java/net/treelzebub/zinepress/api/model/PocketArticle.java
+++ app/src/main/java/net/treelzebub/zinepress/api/model/PocketArticle.java
@@ -1,57 +1,48 @@
 package net.treelzebub.zinepress.api.model;
 
-import android.support.annotation.NonNull;
-
 import com.google.gson.annotations.SerializedName;
-
-import net.treelzebub.zinepress.db.articles.IArticle;
-
-import org.jetbrains.annotations.NotNull;
 
 /**
  * Created by Tre Murillo on 1/3/16
  */
-public class PocketArticle implements IArticle {
+public class PocketArticle {
 
     @SerializedName("item_id")
     private final long id;
 
-    @SerializedName("date")
+    @SerializedName("time_added")
     private final long date;
 
     @SerializedName("resolved_title")
     private final String title;
 
     @SerializedName("resolved_url")
-    private final String originalUrl;
+    private final String url;
 
     PocketArticle(long id, long date, String title, String url) {
         this.id = id;
         this.date = date;
         this.title = title;
-        this.originalUrl = url;
-    }
-
-    public long getId() {
-        return id;
+        this.url = url;
     }
 
     public long getDate() {
         return date;
     }
 
-    @NonNull
     public String getTitle() {
         return title;
     }
 
-    @NonNull
-    public String getOriginalUrl() {
-        return originalUrl;
+    public String getUrl() {
+        return "https://getpocket/a/read/" + id;
     }
 
-    @NotNull
-    public String pocketUrl() {
-        return "https://getpocket/a/read/" + id;
+    public long getId() {
+        return id;
+    }
+
+    public String getOriginalUrl() {
+        return url;
     }
 }
