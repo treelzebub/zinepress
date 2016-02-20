04cfc8189121ca1968dcc44c675a552ba05ea729
Tre Murillo <treelzebub@gmail.com>
2016-02-20 01:08:12 -0800

    update content prov

--- app/src/main/java/net/treelzebub/zinepress/db/articles/ArticleContentProvider.kt
+++ app/src/main/java/net/treelzebub/zinepress/db/articles/ArticleContentProvider.kt
@@ -2,15 +2,12 @@ package net.treelzebub.zinepress.db.articles
 
 import android.content.ContentProvider
 import android.content.ContentValues
-import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteDatabaseLockedException
 import android.database.sqlite.SQLiteException
 import android.net.Uri
 import android.util.Log
-import net.treelzebub.zinepress.R
-import net.treelzebub.zinepress.util.BaseInjection
 
 /**
  * Created by Tre Murillo on 1/31/16
@@ -18,23 +15,14 @@ import net.treelzebub.zinepress.util.BaseInjection
 class ArticleContentProvider : ContentProvider() {
 
     companion object {
-        val TAG = ArticleContentProvider::class.java.simpleName
-        fun uri(c: Context = BaseInjection.context, query: String? = null): Uri {
-            return Uri.Builder()
-                    .scheme("content")
-                    .authority(c.getString(R.string.authority_zines))
-                    .query(query)
-                    .build()
-        }
+        private val TAG = ArticleContentProvider::class.java.simpleName
     }
 
     private val helper: ArticlesSQLiteHelper by lazy { ArticlesSQLiteHelper(context) }
     private val readDb: SQLiteDatabase       get() = helper.readableDatabase
     private val writeDb: SQLiteDatabase      get() = helper.writableDatabase
 
-    override fun onCreate(): Boolean {
-        return true
-    }
+    override fun onCreate() = true
 
     override fun query(uri: Uri, projection: Array<out String>?, selection: String?, selectionArgs: Array<out String>?, sortOrder: String?): Cursor? {
         do {
